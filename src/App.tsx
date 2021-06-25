fc0ad4337fc3e606783502410c53703c0a87a65d
Ariel Gerstein <ariel.gers@gmail.com>
2021-06-25 00:17:57 -0300

    Process first snapshot of data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./App.css";
 
+import useBookConnection from "./hooks/useBookConnection";
+
 function App() {
+  const { bookData } = useBookConnection();
+
   return (
     <div className="App">
       <h1>Buy side / bids</h1>
@@ -11,13 +15,29 @@ function App() {
           <th>Size</th>
           <th>Price</th>
         </tr>
-        <tr>
-          <td>total</td>
-          <td>size</td>
-          <td>price</td>
-        </tr>
+        {bookData.bids.map(({ total, amount, price }) => (
+          <tr>
+            <td>{total}</td>
+            <td>{amount}</td>
+            <td>{price}</td>
+          </tr>
+        ))}
       </table>
       <h1>Sell side / asks</h1>
+      <table>
+        <tr>
+          <th>Total</th>
+          <th>Size</th>
+          <th>Price</th>
+        </tr>
+        {bookData.asks.map(({ total, amount, price }) => (
+          <tr>
+            <td>{total}</td>
+            <td>{amount}</td>
+            <td>{price}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
--- src/hooks/useBookConnection.ts
+++ src/hooks/useBookConnection.ts
@@ -1,11 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+type OrderInput = [number, number];
+
+type Order = {
+  price: number;
+  amount: number;
+  total: number;
+};
+
+function processOrders(orders: OrderInput[]): Order[] {
+  let total = 0;
+
+  return orders.map(([price, amount]) => {
+    total = total + amount;
+
+    return {
+      price,
+      amount,
+      total: total,
+    };
+  });
+}
 
 // @TODO: Complete hook
 function useBookConnection() {
-  let socket;
+  let socket: WebSocket;
+
+  const [bookData, setBookData] = useState<{
+    asks: Order[];
+    bids: Order[];
+  }>({
+    asks: [],
+    bids: [],
+  });
 
   useEffect(() => {
-    const socket = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
+    socket = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
 
     socket.addEventListener("open", (event) => {
       // Send message to start connection
@@ -19,9 +49,19 @@ function useBookConnection() {
     });
 
     socket.addEventListener("message", (event) => {
-      console.log("new message data", JSON.parse(event.data));
+      const messageData = JSON.parse(event.data);
+
+      // Initialize book data with snapshot
+      if (messageData.feed === "book_ui_1_snapshot") {
+        setBookData({
+          asks: processOrders(messageData.asks),
+          bids: processOrders(messageData.bids),
+        });
+      }
     });
   }, []);
+
+  return { bookData };
 }
 
 export default useBookConnection;
