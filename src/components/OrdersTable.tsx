88d7ea261bc273d6399295e539e330b8cce4a648
Ariel Gerstein <ariel.gers@gmail.com>
2021-06-27 21:55:49 -0300

    Add total depth visualizer

--- src/components/OrdersTable.tsx
+++ src/components/OrdersTable.tsx
@@ -13,13 +13,17 @@ export default function OrdersTable({
   type,
   orders,
 }: {
-  type: "asks" | "bids";
+  type: "bids" | "asks";
   orders: Order[];
 }) {
   const numberFormatter = new Intl.NumberFormat();
   const priceFormatter = new Intl.NumberFormat("en-US", {
     minimumFractionDigits: 2,
   });
+
+  const isBidsTable = type === "bids";
+  // @TODO: Review what happens if there are no orders
+  const tableTotal = orders[orders.length - 1].total;
 
   return (
     <table className={`${classes.table} ${classes[type]}`}>
@@ -31,15 +35,29 @@ export default function OrdersTable({
         </tr>
       </thead>
       <tbody>
-        {orders.map(({ total, amount, price }) => (
-          <tr key={price}>
-            <td>{numberFormatter.format(total)}</td>
-            <td>{numberFormatter.format(amount)}</td>
-            <td className={classes.priceCell}>
-              {priceFormatter.format(price)}
-            </td>
-          </tr>
-        ))}
+        {orders.map(({ total, amount, price }) => {
+          const totalPercentage = `${(total / tableTotal) * 100}%`;
+
+          return (
+            <tr
+              key={price}
+              style={{
+                background: `
+                  linear-gradient(
+                    ${isBidsTable ? "to left" : "to right"},
+                    ${isBidsTable ? "#3e212c" : "#103839"} ${totalPercentage},
+                    transparent ${totalPercentage}
+                  )`,
+              }}
+            >
+              <td>{numberFormatter.format(total)}</td>
+              <td>{numberFormatter.format(amount)}</td>
+              <td className={classes.priceCell}>
+                {priceFormatter.format(price)}
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
